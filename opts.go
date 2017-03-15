7d548c07263085a5f817e67df63565c822ed376b
gesaleh <gas1474@gmail.com>
2017-03-15 23:27:16 +0100

    Add files via upload

---
+++ opts.go
@@ -0,0 +1,20 @@
+/**
+ * Copyright (c) Mainflux
+ *
+ * Mainflux server is licensed under an Apache license, version 2.0.
+ * All rights not explicitly granted in the Apache license, version 2.0 are reserved.
+ * See the included LICENSE file for more details.
+ * Fork done by G.SALEH for AgriSmart project
+ */
+
+
+
+package main
+
+// Options block for gnatsd server.
+type Options struct {
+	Trace         bool
+	Debug         bool
+	Logtime       bool
+	LogFile       string
+}
